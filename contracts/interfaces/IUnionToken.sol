787d5e2af83bd9a8e3c84dc55c1a0070c76d74ed
Max Weng <max.m.weng@gmail.com>
2023-08-16 18:06:50 +0800

    fix: incorrect license declaring

--- contracts/interfaces/IUnionToken.sol
+++ contracts/interfaces/IUnionToken.sol
@@ -1,4 +1,4 @@
-//SPDX-License-Identifier: UNLICENSED
+//SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
@@ -16,7 +16,10 @@ interface IUnionToken {
 
     function balanceOf(address account) external view returns (uint256);
 
-    function transfer(address recipient, uint256 amount) external returns (bool);
+    function transfer(
+        address recipient,
+        uint256 amount
+    ) external returns (bool);
 
     function mint(address account, uint256 amount) external returns (bool);
 
@@ -26,7 +29,10 @@ interface IUnionToken {
      *  @param blockNumber The block number to get the vote balance at
      *  @return The number of votes the account had as of the given block
      */
-    function getPriorVotes(address account, uint256 blockNumber) external view returns (uint256);
+    function getPriorVotes(
+        address account,
+        uint256 blockNumber
+    ) external view returns (uint256);
 
     /**
      *  @dev Allows to spend owner's Union tokens by the specified spender.
